cee6aad9deffd22e62c395220d58e0af2a777dec
Darion <pixelarraymedia@gmail.com>
2023-01-05 15:55:52 -0800

    Submission :  Reduce array,grow result

---
+++ 8kyu/ReduceGrow.js
@@ -0,0 +1,16 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// Multiply all values together in order and return the result
+// Using .reduce() we can pass the return value from each element in the array in order
+
+
+const grow = x => {
+    let result = 1;
+    for ( let i = 0; i < x.length; i++ )
+    {
+     result *= x[1];
+    }
+    return result;
+};
