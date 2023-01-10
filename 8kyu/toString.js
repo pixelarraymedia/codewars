e34cc955ba85a2b25a4b19c2bfb783adab34dd6d
Darion <pixelarraymedia@gmail.com>
2023-01-10 15:48:18 -0800

    Submission : to string codewars challenge

---
+++ 8kyu/toString.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given
+
+// 1, 0, 
+// true , flase, string
+
+function booleanToString(b){
+    return b.toString();
+    }
+    
+    booleanToString(true) // 
