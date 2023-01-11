8e9b11b2313646fecbb6dde0545edc4367da4ca4
Darion <pixelarraymedia@gmail.com>
2023-01-11 14:37:55 -0800

    Update : ComplientaryDNA Javascript codewars

--- 8kyu/GradeBook.js
+++ 8kyu/GradeBook.js
@@ -16,5 +16,5 @@
 // Use math.floor we return the number greated than or equal to the grade
 
 function getGrade (s1, s2, s3) {
-    return {10: 'A', 9: 'A', 8: 'B', 7: 'C', 6: 'D'}[Math.floor((s1 + s2 + s3) / 30)] || "F" 
+    return {10: 'A', 9: 'A', 8: 'B', 7: 'C', 6: 'D'}[Math.floor((s1 + s2 + s3) / 3)] || "F" 
   }
