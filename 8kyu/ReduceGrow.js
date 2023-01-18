053178e810a3d0342a0d2193295efa58924b9975
Darion <pixelarraymedia@gmail.com>
2023-01-18 15:28:41 -0800

    update +2 :  code wars sum arrays + Reduce grow one liner

--- 8kyu/ReduceGrow.js
+++ 8kyu/ReduceGrow.js
@@ -5,12 +5,13 @@
 // Multiply all values together in order and return the result
 // Using .reduce() we can pass the return value from each element in the array in order
 
+const grow=x=> x.reduce((a,b) => a*b);
 
-const grow = x => {
-    let result = 1;
-    for ( let i = 0; i < x.length; i++ )
-    {
-     result *= x[1];
-    }
-    return result;
-};
+// const grow = x => {
+//     let result = 1;
+//     for ( let i = 0; i < x.length; i++ )
+//     {
+//      result *= x[1];
+//     }
+//     return result;
+// };
