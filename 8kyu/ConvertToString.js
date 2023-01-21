cec97977a6275778b6be765957863969627da046
Darion <pixelarraymedia@gmail.com>
2023-01-21 13:53:30 -0800

    Update : added String conversion code challenge

---
+++ 8kyu/ConvertToString.js
@@ -0,0 +1,22 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+// Number, + , 0 , 
+// String, Yes, No, console
+
+function boolToWord(bool){
+    // check if number is greater than 0
+    // Boolean will return 1 > return false
+  
+  
+    return bool == true ? "Yes" : "No" 
+  
+  
+  //   if (typeof bool === "true") {
+  //     return "yes"
+  //   } else{ 
+    
+  //   return "false" 
+  //   }
+    
+  }
