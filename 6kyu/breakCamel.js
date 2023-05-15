ee6f1fb50c992accf6614d2f7e6dbd205ea8d297
Darion <pixelarraymedia@gmail.com>
2023-05-15 14:06:07 -0700

    review : camel case function with string

--- 6kyu/breakCamel.js
+++ 6kyu/breakCamel.js
@@ -8,17 +8,25 @@
 // complete the function
 
 function solution(string) {
-    let splitStr = string.split("");
-    let newStr = "";
-  splitStr.forEach(e =>{ 
-    if( e === e.toUpperCase()) newStr += " " + e;
-    else newStr += e;
-    
+  let splitStr = string.split(""); // convert the input string to an array of characters
+  let newStr = ""; // initialize an empty string to store the modified string
+  
+  splitStr.forEach(index => { // loop through each character in the array using the forEach() method
+    if (index === index.toUpperCase()) // if the character is uppercase
+      newStr += " " + index; // add a space followed by the character to the new string
+    else
+      newStr += e; // otherwise, add the character to the new string without any modification
   });
   
-  return newStr;
-  
-  }                 
+  return newStr; // return the modified string
+}
+// In this implementation, we convert the input string to an array of characters using the split() method. We then initialize an empty string newStr to store the modified string.
+
+// We loop through each character in the array using the forEach() method. For each character
+
+
+
+                
   // My solution :
 //   function solution(string) {
 //   let newStr = "";
