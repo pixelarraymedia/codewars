565ee75cedf80c423d92943c4a85f0f5a146522d
Darion <pixelarraymedia@gmail.com>
2023-08-11 06:38:01 -0700

    codewars update: array pos

--- 8kyu/ArrayPosSum.js
+++ 8kyu/ArrayPosSum.js
@@ -24,6 +24,6 @@ function positiveSum(arr) {
   }
 
 
-//   function positiveSum(arr) {
-//     return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
-//   }
+ function positiveSum(arr) {
+    return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
+ }
