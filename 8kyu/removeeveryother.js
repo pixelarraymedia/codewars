ca07a0ff226b9e850acb44f3ac49b3b2a975177e
Darion <pixelarraymedia@gmail.com>
2023-08-15 05:23:20 -0700

    codewars update: return odd elements

---
+++ 8kyu/removeeveryother.js
@@ -0,0 +1,23 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+
+// array of words, never empty, no numbers, words 
+// array, return even elements
+function removeEveryOther(arr){
+    // create result empty array
+    // Loop through strings remove each odd string by storing in an object
+    // return the result
+    
+    let result = [];
+    
+    for(let i = 0; i < arr.length; i += 2){
+     result.push(arr[i]);
+    }
+    return result
+      
+  }
