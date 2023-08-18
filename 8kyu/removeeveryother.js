ee4dbebe97224532a268bb0e39182a56ba805447
Darion <pixelarraymedia@gmail.com>
2023-08-17 21:57:36 -0700

    codewars update

--- 8kyu/removeeveryother.js
+++ 8kyu/removeeveryother.js
@@ -21,3 +21,10 @@ function removeEveryOther(arr){
     return result
       
   }
+
+
+  function removeEveryOther(arr){
+    return arr.filter(function(elem, index) {
+      return index % 2 === 0;
+    });
+  }
--- 8kyu/toString.js
+++ 8kyu/toString.js
@@ -11,3 +11,14 @@ function booleanToString(b){
     }
     
     booleanToString(true) // 
+
+    // We need a function that can transform a number (integer) into a string.
+
+    // What ways of achieving this do you know?
+    
+    // Examples (input --> output):
+    // 123  --> "123"
+    // 999  --> "999"
+    // -100 --> "-100"
+
+    const numberToString = num => num.toString()
