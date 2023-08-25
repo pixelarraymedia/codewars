07ba27e35141204dd996cec69a9e38926727e955
Darion <pixelarraymedia@gmail.com>
2023-08-25 03:02:14 -0700

    codewars update, sort array

--- 8kyu/CountPositive.js
+++ 8kyu/CountPositive.js
@@ -24,3 +24,8 @@ function countPositivesSumNegatives(input) {
     
     return [count, neg];
   }
+
+
+  function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
