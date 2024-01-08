0dc20596257e2bb19170abf55e0f59f80724024f
Darion <pixelarraymedia@gmail.com>
2024-01-08 04:52:50 -0800

    codewars update, sort array based on length

---
+++ 7kyu/sortarray.js
@@ -0,0 +1,17 @@
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+const sortByLength = array => array.sort((a,b) => a.length - b.length)
+
+// if the result is negative A is set to A lower index than B. meaning A will be indexed in a position before B
+// if the result is positive A is sorted to an higher index then B. Meaning A will be indexed in a position after B. 
+// if the A - B  result 0 they will remain in their current order
