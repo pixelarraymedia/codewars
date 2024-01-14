0827b4f4d0f7c0878501fb96d31305632e72063b
Darion <pixelarraymedia@gmail.com>
2024-01-14 04:13:00 -0800

    codewars update, min max

--- 7kyu/countdigits.js
+++ 7kyu/countdigits.js
@@ -34,3 +34,12 @@ function nbDig(n,d) {
     return count;
 
 }
+
+// best practice
+function nbDig(n, d) {
+    var res=0;
+        for (var g=0;g<=n;g++){
+          var square=(g*g+"").split("");
+          square.forEach((s)=>s==d?res++:null)
+        }return res;
+    }
