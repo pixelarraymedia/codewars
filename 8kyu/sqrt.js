9ec23b597447e236dcc94f32388fa1ed9e50f058
Darion <pixelarraymedia@gmail.com>
2024-03-12 22:46:40 -0700

    codewars update. Leetcode and square root javascript codewars

---
+++ 8kyu/sqrt.js
@@ -0,0 +1,8 @@
+// Instructions
+// Output
+// Now you have to write a function that takes an argument and returns the square of it.
+
+const square = n => n * n;
+
+// clever
+const square = n => Math.pow(n, 2)
