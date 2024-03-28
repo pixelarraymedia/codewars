7a30012a26f8b72401fa63c116931009a36a2308
Darion <pixelarraymedia@gmail.com>
2024-03-28 05:12:08 -0700

    codewars challege: total cost over an input of days

--- 8kyu/transportation.js
+++ 8kyu/transportation.js
@@ -5,6 +5,7 @@
 // Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
 // Write a code that gives out the total amount for different days(d).
+
 // my solution
 const rentalCarCost = (d) => {
   
